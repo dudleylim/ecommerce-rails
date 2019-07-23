df1b2def78e43446e18a0228df90c858c3b9455b
nidgefalcon <jose.falcon@obf.ateneo.edu>
2019-07-24 01:10:11 +0800

    Adding Background Test Image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+   #Code that supposedly fixes bg images
+  #https://stackoverflow.com/questions/21974190/background-image-not-showing-up-in-heroku
+  config.serve_static_assets = true
+  config.assets.digest = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -27,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
