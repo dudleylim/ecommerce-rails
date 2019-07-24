893131a71e5dd0fbd5038aa7346c32cf86e0714d
nidgefalcon <jose.falcon@obf.ateneo.edu>
2019-07-24 13:01:28 +0800

    fixed models

--- app/models/admin.rb
+++
@@ -1,4 +0,0 @@
-class Admin < ApplicationRecord
-	validates :email, presence: true
-	validates :password_digest, presence: true
-end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
     has_many :products
     validates :name, uniqueness: true, presence:true
-    validates :password, presence:true
 end
