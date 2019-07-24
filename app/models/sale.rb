3ec8037ae77bbb2ef9d4f29e73a8fd49ad5c18c8
dudleylim <dudley.lim@obf.ateneo.edu>
2019-07-24 13:58:01 +0700

    asdasd

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,5 +4,4 @@ class Sale < ApplicationRecord
     belongs_to :seller
     validates :seller_id, presence: true
     validates :price, presence: true
-    validates :category_id, presence: true
 end
